7c4a1ea31dc2542a2a0f88f23bc779d88f44fe83
congzw <2981810300@qq.com>
2019-09-06 09:13:51 +0800

    add RuntimeHelper

---
+++ src/Common/RuntimeHelper.cs
@@ -0,0 +1,50 @@
+﻿using System.Runtime.InteropServices;
+// ReSharper disable CheckNamespace
+
+namespace Common
+{
+    public class RuntimeHelper
+    {
+        public bool IsOsPlatform(params OSPlatform[] osPlatforms)
+        {
+            foreach (var osPlatform in osPlatforms)
+            {
+                if (RuntimeInformation.IsOSPlatform(osPlatform))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public bool IsProcessArchitecture(params Architecture[] architectures)
+        {
+            foreach (var architecture in architectures)
+            {
+                if (RuntimeInformation.ProcessArchitecture == architecture)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public bool IsOsArchitecture(params Architecture[] architectures)
+        {
+            foreach (var architecture in architectures)
+            {
+                if (RuntimeInformation.OSArchitecture == architecture)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        #region for simple use
+
+        public static RuntimeHelper Instance = new RuntimeHelper();
+
+        #endregion
+    }
+}
