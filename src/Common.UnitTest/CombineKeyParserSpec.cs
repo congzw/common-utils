add3a26a71c3afd4d7d0e613fe40b85b57f95e1a
congzw <2981810300@qq.com>
2019-10-14 16:12:01 +0800

    add CombineKeyParser and Spec

---
+++ src/Common.UnitTest/CombineKeyParserSpec.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Common
+{
+    [TestClass]
+    public class CombineKeyParserSpec
+    {
+        [TestMethod]
+        public void CreateCombinedKey_NullOrEmpty_ShouldReturnDefault()
+        {
+            var parser = CombineKeyParser.NewForTest();
+            parser.CreateCombinedKey(null).ShouldEqual(string.Empty);
+            parser.CreateCombinedKey(new Dictionary<string, object>()).ShouldEqual(string.Empty);
+        }
+
+        [TestMethod]
+        public void CreateCombinedKey_AllKey_ShouldInclude()
+        {
+            var parser = CombineKeyParser.NewForTest();
+            var dic = new Dictionary<string, object>();
+            dic.Add("A", "a");
+            dic.Add("B", "");
+            dic.Add("C", "c");
+            parser.CreateCombinedKey(dic).ShouldEqual("A=a&B=&C=c");
+        }
+        
+        [TestMethod]
+        public void ParseCombinedKey__NullOrEmpty_ShouldReturnDefault()
+        {
+            var parser = CombineKeyParser.NewForTest();
+            parser.ParseCombinedKey(null).Count.ShouldEqual(0);
+            parser.ParseCombinedKey("").Count.ShouldEqual(0);
+            parser.ParseCombinedKey(" ").Count.ShouldEqual(0);
+        }
+
+        [TestMethod]
+        public void ParseCombinedKey_AllKey_ShouldInclude()
+        {
+            var parser = CombineKeyParser.NewForTest();
+            var dic = parser.ParseCombinedKey("A=a&B=&c=c");
+            dic.Count.ShouldEqual(3);
+            dic["A"].ShouldEqual("a");
+            dic["B"].ShouldEqual("");
+            dic["C"].ShouldEqual("c");
+            dic["c"].ShouldEqual("c");
+        }
+        
+        [TestMethod]
+        public void AutoGetCombinedKey_ObjectExt_ShouldOk()
+        {
+            var parser = CombineKeyParser.NewForTest();
+            var foo = new CombineKeyFoo();
+            parser.AutoGetCombinedKey(foo, "A", "b").ShouldEqual("A=a&b=1");
+        }
+
+        [TestMethod]
+        public void AutoGetCombinedKey_DicExt_ShouldOk()
+        {
+            var parser = CombineKeyParser.NewForTest();
+            var items = new Dictionary<string, object>();
+            items.Add("A", "a");
+            items.Add("b", 1);
+            items.Add("C", "c");
+            parser.AutoGetCombinedKey(items,  "A", "B").ShouldEqual("A=a&B=1");
+        }
+        
+        [TestMethod]
+        public void AutoSetCombinedKey_ObjectExt_ShouldOk()
+        {
+            var parser = CombineKeyParser.NewForTest();
+            var foo = new CombineKeyFoo();
+            parser.AutoSetCombinedKey(foo, "A", "aa");
+            parser.AutoSetCombinedKey(foo, "B", 11);
+            foo.A.ShouldEqual("aa");
+            foo.B.ShouldEqual(11);
+        }
+
+        [TestMethod]
+        public void AutoSetCombinedKey_DicExt_ShouldOk()
+        {
+            var parser = CombineKeyParser.NewForTest();
+            var items = new Dictionary<string, object>();
+            items.Add("A", "a");
+            items.Add("b", 1);
+            items.Add("C", "c");
+
+            parser.AutoSetCombinedKey(items, "A", "aa");
+            parser.AutoSetCombinedKey(items, "B", 11);
+            parser.AutoSetCombinedKey(items, "D", "d");
+
+            items["A"].ShouldEqual("aa");
+            items["b"].ShouldEqual(11);
+            items["C"].ShouldEqual("c");
+        }
+
+        public class CombineKeyFoo
+        {
+            public CombineKeyFoo()
+            {
+                A = "a";
+                B = 1;
+            }
+
+            public string A { get; set; }
+            public int B { get; set; }
+            public string X { get; set; }
+            public int[] Y { get; set; }
+        }
+    }
+}
---
+++ src/Common/CombineKeyParser.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text.RegularExpressions;
+
+namespace Common
+{
+    public interface ICombineKeyParser
+    {
+        string CreateCombinedKey(IDictionary<string, object> items);
+        IDictionary<string, string> ParseCombinedKey(string combinedKey);
+    }
+
+    public class CombineKeyParser : ICombineKeyParser
+    {
+        private CombineKeyParser()
+        {
+            
+        }
+
+        public string CreateCombinedKey(IDictionary<string, object> items)
+        {
+            if (items == null || items.Count == 0)
+            {
+                return string.Empty;
+            }
+
+            var segments = from item in items
+                select string.Format("{0}={1}", item.Key, item.Value);
+            return string.Join("&", segments);
+        }
+
+        public IDictionary<string, string> ParseCombinedKey(string combinedKey)
+        {
+            var dic = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase);
+            if (string.IsNullOrWhiteSpace(combinedKey))
+            {
+                return dic;
+            }
+
+            if (string.IsNullOrWhiteSpace(combinedKey))
+            {
+                return dic;
+            }
+
+            foreach (string itemKeyValue in Regex.Split(combinedKey, "&"))
+            {
+                string[] singlePair = Regex.Split(itemKeyValue, "=");
+                var key = singlePair[0];
+                var value = singlePair.Length == 2 ? singlePair[1] : string.Empty;
+                dic.Add(key, value);
+            }
+            return dic;
+        }
+        
+        #region for ut & di extensions
+
+        public static CombineKeyParser NewForTest()
+        {
+            return new CombineKeyParser();
+        }
+
+        public static Func<ICombineKeyParser> Resolve { get; } = SimpleLazyFactory<ICombineKeyParser>.Instance.Default(() => new CombineKeyParser()).Resolve;
+
+        #endregion
+    }
+
+    public static class CombineKeyParserExtensions
+    {
+        public static string AutoGetCombinedKey(this ICombineKeyParser parser, IDictionary<string, object> items, params string[] includeKeys)
+        {
+            var includedItems = SelectIncludeItems(items, includeKeys);
+            return parser.CreateCombinedKey(includedItems);
+        }
+        public static string AutoGetCombinedKey(this ICombineKeyParser parser, object model, params string[] includeKeys)
+        {
+            if (model == null)
+            {
+                return null;
+            }
+
+            var propertiesDic = GetGetPropertiesDic(model);
+            return parser.AutoGetCombinedKey(propertiesDic, includeKeys);
+        }
+
+        public static IDictionary<string, object> AutoSetCombinedKey(this ICombineKeyParser parser, IDictionary<string, object> model, IDictionary<string, object> items, params string[] includeKeys)
+        {
+            if (model == null)
+            {
+                return null;
+            }
+            var includedItems = SelectIncludeItems(items, includeKeys);
+            foreach (var includedItem in includedItems)
+            {
+                if (model.ContainsKey(includedItem.Key))
+                {
+                    model[includedItem.Key] = includedItem.Value;
+                }
+            }
+            return model;
+        }
+        public static object AutoSetCombinedKey(this ICombineKeyParser parser, object model, IDictionary<string, object> items, params string[] includeKeys)
+        {
+            if (model == null)
+            {
+                return null;
+            }
+            var includedItems = SelectIncludeItems(items, includeKeys);
+            foreach (var includedItem in includedItems)
+            {
+                SetProperty(model, includedItem.Key, includedItem.Value);
+            }
+            return model;
+        }
+
+        public static IDictionary<string, object> AutoSetCombinedKey(this ICombineKeyParser parser, IDictionary<string, object> model, string key, object value)
+        {
+            if (model == null)
+            {
+                return null;
+            }
+
+            var theKey = model.Keys.SingleOrDefault(x => x.Equals(key, StringComparison.OrdinalIgnoreCase));
+            if (theKey != null)
+            {
+                model[theKey] = value;
+            }
+
+            return model;
+        }
+        public static object AutoSetCombinedKey(this ICombineKeyParser parser, object model, string key, object value)
+        {
+            if (model == null)
+            {
+                return null;
+            }
+            SetProperty(model, key, value);
+            return model;
+        }
+
+        private static IDictionary<string, object> GetGetPropertiesDic(object model)
+        {
+            var result = new Dictionary<string, object>();
+            if (model == null)
+            {
+                return result;
+            }
+
+            var theType = model.GetType();
+            var propertyInfos = theType.GetProperties();
+            foreach (var propertyInfo in propertyInfos)
+            {
+                result.Add(propertyInfo.Name, propertyInfo.GetValue(model, null));
+            }
+            return result;
+        }
+        private static IDictionary<string, T> SelectIncludeItems<T>(IDictionary<string, T> items, params string[] includeKeys)
+        {
+            var result = new Dictionary<string, T>(StringComparer.OrdinalIgnoreCase);
+            if (items == null || items.Count == 0)
+            {
+                return result;
+            }
+
+            if (includeKeys == null || includeKeys.Length == 0)
+            {
+                return items;
+            }
+
+
+            foreach (var includeKey in includeKeys)
+            {
+                foreach (var item in items)
+                {
+                    if (item.Key.Equals(includeKey, StringComparison.OrdinalIgnoreCase))
+                    {
+                        result.Add(includeKey, item.Value);
+                    }
+                }
+            }
+
+            return result;
+        }
+        private static bool SetProperty(object model, string key, object value)
+        {
+            var result = false;
+            if (model != null && !string.IsNullOrEmpty(key) && value != null)
+            {
+                //获取类型信息
+                var theType = model.GetType();
+                var propertyInfos = theType.GetProperties();
+
+                foreach (var propertyInfo in propertyInfos)
+                {
+                    if (propertyInfo.Name.Equals(key, StringComparison.OrdinalIgnoreCase))
+                    {
+                        var theValue = value;
+                        if (value.GetType() != propertyInfo.PropertyType)
+                        {
+                            theValue = Convert.ChangeType(value, propertyInfo.PropertyType);
+                        }
+                        propertyInfo.SetValue(model, theValue, null);
+                        result = true;
+                        break;
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
