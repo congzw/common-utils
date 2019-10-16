b5d72174c3965c8c396b7c18832956693fc99609
congzw <2981810300@qq.com>
2019-10-16 17:44:23 +0800

    add TowKeyMapping and Spec

---
+++ src/Common.UnitTest/TowKeyMappingSpec.cs
@@ -0,0 +1,54 @@
+﻿using Common.Common.ExtTest;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Common
+{
+    [TestClass]
+    public class TowKeyMappingSpec
+    {
+        [TestMethod]
+        public void TryGetLogicConnectionId_Should_Ok()
+        {
+            var towKeyMapping = new TowKeyMapping();
+            towKeyMapping.SetMappingWithIdAndLogicId("A", "1");
+            towKeyMapping.SetMappingWithIdAndLogicId("B", "2");
+
+            towKeyMapping.TryGetLogicConnectionId("a").ShouldEqual("1");
+            towKeyMapping.TryGetConnectionId("1").ShouldEqual("A");
+            
+            towKeyMapping.TryGetLogicConnectionId("B").ShouldEqual("2");
+            towKeyMapping.TryGetConnectionId("2").ShouldEqual("B");
+
+            towKeyMapping.TryGetLogicConnectionId("C").ShouldEqual(null);
+            towKeyMapping.TryGetConnectionId("3").ShouldEqual(null);
+        }
+    }
+    
+    namespace Common.ExtTest
+    {
+        #region demo
+
+        public static class DoubleKeyMappingExtensions
+        {
+            //Key1: ConnectionId
+            //Key2: LogicConnectionId
+
+            public static void SetMappingWithIdAndLogicId(this TowKeyMapping mapping, string connectionId, string logicConnectionId)
+            {
+                mapping?.AddMapping(connectionId, logicConnectionId);
+            }
+
+            public static string TryGetLogicConnectionId(this TowKeyMapping mapping, string connectionId)
+            {
+                return mapping?.TryGetKey2(connectionId);
+            }
+
+            public static string TryGetConnectionId(this TowKeyMapping mapping, string logicConnectionId)
+            {
+                return mapping?.TryGetKey1(logicConnectionId);
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ src/Common/TowKeyMapping.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+
+// ReSharper disable CheckNamespace
+namespace Common
+{
+    public class TowKeyMapping
+    {
+        public IDictionary<string, string> Dic12 { get; set; }
+
+        public IDictionary<string, string> Dic21 { get; set; }
+
+        public TowKeyMapping()
+        {
+            Dic12 = new ConcurrentDictionary<string, string>(StringComparer.OrdinalIgnoreCase);
+            Dic21 = new ConcurrentDictionary<string, string>(StringComparer.OrdinalIgnoreCase);
+        }
+        
+        public void AddMapping(string key1, string key2)
+        {
+            Dic12[key1] = key2;
+            Dic21[key2] = key1;
+        }
+
+        public string TryGetKey1(string key2)
+        {
+            Dic21.TryGetValue(key2, out var key1);
+            return key1;
+        }
+
+        public string TryGetKey2(string key1)
+        {
+            Dic12.TryGetValue(key1, out var key2);
+            return key2;
+        }
+    }
+
+    #region demo
+
+    //public static class DoubleKeyMappingExtensions
+    //{
+    //    //Key1: ConnectionId
+    //    //Key2: LogicConnectionId
+
+    //    public static void SetMappingWithIdAndLogicId(this TowKeyMapping mapping, string connectionId, string logicConnectionId)
+    //    {
+    //        mapping?.AddMapping(connectionId, logicConnectionId);
+    //    }
+
+    //    public static string TryGetLogicConnectionId(this TowKeyMapping mapping, string connectionId)
+    //    {
+    //        return mapping?.TryGetKey2(connectionId);
+    //    }
+
+    //    public static string TryGetConnectionId(this TowKeyMapping mapping, string logicConnectionId)
+    //    {
+    //        return mapping?.TryGetKey1(logicConnectionId);
+    //    }
+    //}
+    #endregion
+}
