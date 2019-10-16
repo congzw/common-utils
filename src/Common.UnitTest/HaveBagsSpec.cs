6ed440a8cb6df447ad61986fe121ce79661b0d5e
congzw <2981810300@qq.com>
2019-10-16 12:05:05 +0800

    temp save

---
+++ src/Common.UnitTest/HaveBagsSpec.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Common
+{
+    [TestClass]
+    public class HaveBagsSpec
+    {
+        [TestMethod]
+        public void Bags_Tell_ShouldOk()
+        {
+            var myBag = new Foo();
+            myBag.SetBagValue("A", 1);
+            myBag.GetBagValue("a", 0).ShouldEqual(1);
+        }
+
+        //[TestMethod]
+        //public void Bags_Implicit_ShouldOk()
+        //{
+        //    var myBag = new Bar();
+        //    myBag.SetBagValue("A", 1);
+        //    myBag.GetBagValue("a", 0).ShouldEqual(1);
+        //}
+
+        [TestMethod]
+        public void Bags_Explicit_ShouldOk()
+        {
+            var myBag = new Foo();
+            myBag.SetBagValue("A", 1);
+            myBag.GetBagValue("a", 0).ShouldEqual(1);
+        }
+
+        public class Foo : IHaveBagsProperty
+        {
+            public Foo()
+            {
+                Items = new ConcurrentDictionary<string, object>(StringComparer.OrdinalIgnoreCase);
+            }
+
+            public IDictionary<string, object> Items { get; set; }
+
+            public string GetBagsPropertyName()
+            {
+                return nameof(Items);
+            }
+        }
+
+        //public class Bar
+        //{
+        //    public Bar()
+        //    {
+        //        Bags = new ConcurrentDictionary<string, object>(StringComparer.OrdinalIgnoreCase);
+        //    }
+        //    public IDictionary<string, object> Bags { get; set; }
+        //}
+
+        public class Blah : IHaveBags
+        {
+            public Blah()
+            {
+                Bags = new ConcurrentDictionary<string, object>(StringComparer.OrdinalIgnoreCase);
+            }
+            public IDictionary<string, object> Bags { get; set; }
+        }
+    }
+}
---
+++ src/Common/IHaveBags.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+// ReSharper disable CheckNamespace
+
+// change list:
+// 20191016 first release 0.0.1
+namespace Common
+{
+    public interface IHaveBags
+    {
+        //IDictionary<string, object> Bags { get; set; }
+    }
+
+    public interface IHaveBagsProperty : IHaveBags
+    {
+        string GetBagsPropertyName();
+    }
+
+    public static class HaveBagsExtensions
+    {
+        //public static T SetBagValue<T>(this T instance, string key, object value)
+        //{
+        //    if (instance == null)
+        //    {
+        //        return instance;
+        //    }
+
+        //    var bags = GetBagsRef(instance);
+        //    bags[key] = value;
+        //    return instance;
+        //}
+
+        //public static TValue GetBagValue<T, TValue>(this T instance, string key, TValue defaultValue)
+        //{
+        //    var bags = GetBagsRef(instance);
+
+        //    if (!bags.ContainsKey(key))
+        //    {
+        //        return defaultValue;
+        //    }
+
+        //    return (TValue)bags[key];
+        //}
+
+        //private static IDictionary<string, object> GetBagsRef<T>(T instance)
+        //{
+        //    IDictionary<string, object> bags = null;
+        //    var bagName = string.Empty;
+        //    try
+        //    {
+        //        bagName = instance is IHaveBagsProperty ? ((IHaveBagsProperty)instance).GetBagsPropertyName() : "Bags";
+        //        bags = GetProperty(instance, bagName) as IDictionary<string, object>;
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        Console.WriteLine(ex);
+        //    }
+        //    if (bags == null)
+        //    {
+        //        throw new InvalidOperationException(string.Format("没有找到名为{0}的Bags属性", bagName));
+        //    }
+
+        //    return bags;
+        //}
+
+        public static T SetBagValue<T>(this T instance, string key, object value) where T : IHaveBags
+        {
+            if (instance == null)
+            {
+                return instance;
+            }
+
+            var bags = GetBags(instance);
+            bags[key] = value;
+            return instance;
+        }
+
+        public static TValue GetBagValue<T, TValue>(this T instance, string key, TValue defaultValue) where T : IHaveBags
+        {
+            var bags = GetBags(instance);
+
+            if (!bags.ContainsKey(key))
+            {
+                return defaultValue;
+            }
+
+            return (TValue)bags[key];
+        }
+
+        private static IDictionary<string, object> GetBags<T>(T instance) where T : IHaveBags
+        {
+            IDictionary<string, object> bags = null;
+            var bagName = string.Empty;
+            try
+            {
+                bagName = instance is IHaveBagsProperty ? ((IHaveBagsProperty)instance).GetBagsPropertyName() : "Bags";
+                bags = GetProperty(instance, bagName) as IDictionary<string, object>;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            if (bags == null)
+            {
+                throw new InvalidOperationException(string.Format("没有找到名为{0}的Bags属性", bagName));
+            }
+
+            return bags;
+        }
+
+
+        private static object GetProperty(object target, string name)
+        {
+            var site = System.Runtime.CompilerServices.
+                CallSite<Func<System.Runtime.CompilerServices.CallSite, object, object>>
+                .Create(Microsoft.CSharp.RuntimeBinder.Binder.GetMember(0, name, target.GetType(),
+                    new[] { Microsoft.CSharp.RuntimeBinder.CSharpArgumentInfo.Create(0, null) }));
+            return site.Target(site, target);
+        }
+    }
+}
