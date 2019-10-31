5435bf3c611b60dc89be7c0f44c69c9d48c48903
congzw <2981810300@qq.com>
2019-10-31 11:31:08 +0800

    save group

--- src/Common.SignalR/ClientMonitors/Groups/IClientGroupManager.cs
+++ src/Common.SignalR/ClientMonitors/Groups/IClientGroupManager.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace Common.SignalR.ClientMonitors.Groups
 {
@@ -6,5 +7,28 @@ namespace Common.SignalR.ClientMonitors.Groups
     {
         Task AddToGroup(AddToGroup args);
         Task RemoveFromGroup(RemoveFromGroup args);
+        Task<IList<ScopeClientGroup>> GetGroups(IScopeClientGroupLocate args);
+    }
+
+    public class ScopeClientGroup : IScopeClientGroupLocate
+    {
+        public string ScopeId { get; set; }
+        public string ClientId { get; set; }
+        public string Group { get; set; }
+    }
+
+    public interface IScopeClientGroupRepository
+    {
+        IEnumerable<ScopeClientGroup> QueryScopeClientGroups(IScopeClientGroupLocate args);
+        ScopeClientGroup GetScopeClientGroup(IScopeClientGroupLocate args);
+        void Add(ScopeClientGroup scopeClientGroup);
+        void Remove(ScopeClientGroup scopeClientGroup);
+    }
+
+    public class GetScopeClientGroupsArgs : IScopeClientGroupLocate
+    {
+        public string ScopeId { get; set; }
+        public string ClientId { get; set; }
+        public string Group { get; set; }
     }
 }
