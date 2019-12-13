50331cad8324c4e337c547a7d5bf9ba2c512070b
congzw <2981810300@qq.com>
2019-12-13 11:27:02 +0800

    add SimpleConvert

---
+++ src/Common/SimpleConvert.cs
@@ -0,0 +1,45 @@
+﻿using System;
+// ReSharper disable CheckNamespace
+
+namespace Common
+{
+    public interface ISafeConvert
+    {
+        T SafeConvertTo<T>(object value);
+    }
+
+    public static class SimpleConvert
+    {
+        private static Func<ISafeConvert> _safeConvertFunc;
+
+        public static ISafeConvert Current => _safeConvertFunc?.Invoke();
+
+        public static void Initialize(Func<ISafeConvert> safeConvertFunc)
+        {
+            _safeConvertFunc = safeConvertFunc ?? throw new ArgumentNullException(nameof(safeConvertFunc));
+        }
+    }
+
+    ////how to use:
+    ////SimpleConvert.Initialize(() => SafeConvert.Instance);
+    //public class SafeConvert : ISafeConvert
+    //{
+    //    public T SafeConvertTo<T>(object value)
+    //    {
+    //        if (value is T modelValue)
+    //        {
+    //            return modelValue;
+    //        }
+    //        //处理网络序列化
+    //        if (value is JObject theJObject)
+    //        {
+    //            return theJObject.ToObject<T>();
+    //        }
+    //        var json = JsonConvert.SerializeObject(value);
+    //        var argsT = JsonConvert.DeserializeObject<T>(json);
+    //        return argsT;
+    //    }
+
+    //    public static SafeConvert Instance = new SafeConvert();
+    //}
+}
