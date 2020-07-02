9f2c5d4b46e5da873c00183f1be643520ad6718d
congzw <2981810300@qq.com>
2020-07-02 10:38:57 +0800

    add readme

--- src/Common/Scopes/ScopeContext.cs
+++ src/Common/Scopes/ScopeContext.cs
@@ -3,6 +3,20 @@ using System.Collections.Generic;
 
 namespace Common.Scopes
 {
+    /// <summary>
+    /// 逻辑划分范围唯一标识
+    /// </summary>
+    public interface IScopeId
+    {
+        /// <summary>
+        /// 逻辑划分范围唯一标识，例如：按教室划分等场景
+        /// </summary>
+        string ScopeId { get; set; }
+    }
+
+    /// <summary>
+    /// 范围上下文
+    /// </summary>
     public class ScopeContext : IScopeId
     {
         public string ScopeId { get; set; }
