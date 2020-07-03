7172260bac0322e6d7fae8a70ebee31b806c7a02
congzw <2981810300@qq.com>
2020-07-03 17:59:23 +0800

    add const

--- src/Common.SignalR/SignalR/SignalrExtensions.cs
+++ src/Common.SignalR/SignalR/SignalrExtensions.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Common.Http;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.SignalR;
 
 namespace Common.SignalR
@@ -8,6 +9,21 @@ namespace Common.SignalR
         public static HttpContext TryGetHttpContext(this Hub hub)
         {
             return hub?.Context?.GetHttpContext();
+        }
+
+        public static string TryGetScopeId(this Hub hub)
+        {
+            return hub?.TryGetHttpContext().TryGetQueryParameterValue(HubConst.Args_ScopeId, HubConst.DefaultScopeId);
+        }
+
+        public static string TryGetClientId(this Hub hub)
+        {
+            return hub?.TryGetHttpContext().TryGetQueryParameterValue(HubConst.Args_ClientId, string.Empty);
+        }
+
+        public static string TryGetConnectionId(this Hub hub)
+        {
+            return hub?.TryGetHttpContext().TryGetQueryParameterValue(HubConst.Args_Id, string.Empty);
         }
     }
 }
